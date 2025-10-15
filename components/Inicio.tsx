a588a2a8813ffe845ba41d86807b1224d532a3c9
dev-caw <davison.menezes@caw.adv.br>
2025-10-14 22:45:22 -0300

    Primeiro commit: início do projeto

--- components/Inicio.tsx
+++ components/Inicio.tsx
@@ -24,9 +24,6 @@ const Inicio: React.FC<InicioProps> = ({ onModuleSelect }) => {
         {otherModules.map(moduleName => (
           <ModuleCard key={moduleName} moduleName={moduleName} onModuleSelect={onModuleSelect} />
         ))}
-        <div className="flex items-center justify-center p-6">
-          <p className="text-sm text-slate-500">© 2025 INPI</p>
-        </div>
       </div>
     </div>
   );
